65c67a25f5c9d478a56896ee0e2a621c70d849b3
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-07-31 16:08:09 +0900

    Create (7.31)_variation.java

---
+++ (7.31)_variation.java
@@ -0,0 +1,8 @@
+class Hello {
+    public static void main(String[] args){
+        int x=10,y=20;
+        System.out.println("x:" + x + " y:" + y);
+        System.out.println("x:" + 10 + " y:" + 20);
+    }
+}
+
