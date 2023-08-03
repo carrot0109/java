b3e57b593e1b367b963857d2989da7a326011767
carrot0109 <137762389+carrot0109@users.noreply.github.com>
2023-08-03 16:15:44 +0900

    Create (8.3)_a variety of arr initialization.java

---
+++ (8.3)_a variety of arr initialization.java
@@ -0,0 +1,9 @@
+class Hello {
+
+    public static void main(String[] args){
+        int[] arr = new int[5];             // 갯수 선언 시 new필요
+        int[] arr2 = {1, 2, 3, 4, 5};       // 초기화를 동시에. new사용할 필요없음  
+        int[] arr3 = new int[] {1, 2, 3};   // 위 코드와 동일
+    }
+}
+
